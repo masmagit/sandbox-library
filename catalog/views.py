5d0a1eb4f26c41021f7715ff2334f9c1735ea960
masmagit <masma.web@gmail.com>
2021-05-25 15:10:20 +0200

    part 5 completed

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+    
+    word = "Life"
+    num_books_word = Book.objects.filter(title__icontains=word).count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_word': (word, num_books_word),
+    }
+
+    return render(request, 'catalog/index.html', context=context)
