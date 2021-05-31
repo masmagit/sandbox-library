5f6b8200821cc99ce23fd6bfa823da72f2b5bb0f
masmagit <masma.web@gmail.com>
2021-05-31 20:31:27 +0200

    Part 9 completed

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,6 +7,15 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), {"custom_param" : "some parameter"}, name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('borrowed/', views.LoanedBooksListView.as_view(), name='borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+    path('book/create/', views.BookCreate.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,15 @@
+import datetime
 from django.shortcuts import render, get_object_or_404
-from catalog.models import Book, Author, BookInstance, Genre
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from catalog.models import Book, Author, BookInstance, Genre
+from catalog.forms import RenewBookForm, RenewBookModelForm
 
 class BookListView(generic.ListView):
     model = Book
@@ -60,7 +68,6 @@ class LoanedBooksListView(PermissionRequiredMixin, LoanedBooksByUserListView):
     def get_queryset(self):
         return BookInstance.objects.filter(status__exact='o').order_by('due_back')    
 
-
 def index(request):
     """View function for home page of site."""
     if request.session.test_cookie_worked():
@@ -93,3 +100,71 @@ def index(request):
     }
 
     return render(request, 'catalog/index.html', context=context)
+
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def renew_book_librarian(request, pk):
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = RenewBookModelForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_instance.due_back = form.cleaned_data['due_back']
+            book_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('borrowed') )
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookModelForm(initial={'due_back': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/book_renew_librarian.html', context)
+
+# Generic editing views for Author
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_birth': '01/01/1900'}
+    permission_required = ('catalog.can_mark_returned')
+    # standard template name is "model_name_form.html", _form part can be redefined
+    # template_name_suffix = '_editform' 
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+    permission_required = ('catalog.can_mark_returned')
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+    permission_required = ('catalog.can_mark_returned')
+    # standard template name is "model_name_confirm_delete.htmll"
+
+# Generic editing views for Book
+class BookCreate(PermissionRequiredMixin, CreateView):
+    model = Book
+    fields = '__all__'
+    permission_required = ('catalog.can_mark_returned')
+
+class BookUpdate(PermissionRequiredMixin, UpdateView):
+    model = Book
+    fields = '__all__'
+    permission_required = ('catalog.can_mark_returned')
+
+class BookDelete(PermissionRequiredMixin, DeleteView):
+    model = Book
+    success_url = reverse_lazy('books')
+    permission_required = ('catalog.can_mark_returned')
